95188dc15b93e07cb34c4fb8041f811b15585dee
a-nkunzimana <andymucyo@gmail.com>
2026-02-05 23:00:50 +0100

    feature: moving completed

--- modules/player.py
+++ modules/player.py
@@ -1,11 +1,12 @@
-import pygame # noqa: F401
+import pygame
+from pygame.math import Vector2 # noqa: F401
 from .circleshape import CircleShape
-from .constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
+from .constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation: int = 0
 
     def triangle(self) -> list[int]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -18,16 +19,28 @@ class Player(CircleShape):
     def draw(self, screen):
        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH) 
 
-    def rotate(self, dt: float) -> float :
+    def rotate(self, dt: int) -> int :
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
-    def update(self, dt: float) -> float:
+    def update(self, dt: int) -> int:
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            return self.rotate(dt)
+            return self.rotate(-dt)
 
         if keys[pygame.K_d]:
-            return self.rotate(-dt)
+            return self.rotate(dt)
+
+        if keys[pygame.K_s]:
+            return self.move(-dt)
+
+        if keys[pygame.K_w]:
+            return self.move(dt)
+
+    def move(self, dt:int): 
+        unit_vector: Vector2 = pygame.Vector2(0,1) 
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_vector_with_speed: Vector2 = rotated_vector * PLAYER_SPEED * dt 
+        self.position += rotated_vector_with_speed
 
 
