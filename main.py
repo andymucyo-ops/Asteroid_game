1a1fcd88df3b67469ad1fbc2d4dc5985a8880fce
a-nkunzimana <andymucyo@gmail.com>
2026-02-05 03:19:07 +0100

    feature: game window rendering
    initialized game loop and handle closing game window

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pygame
 from modules.constants import SCREEN_WIDTH,SCREEN_HEIGHT
-import modules.logger # noqa: F401
+from modules.logger import log_state
 
 def main():
     print(f"Starting Asteroids with the pygame version: {pygame.version.ver}")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
